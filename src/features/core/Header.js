1ae3e1fa88f782e0bb8164465c60ea5ec62e25de
mfelfelani72@gmail.com <mfcompany21@gmail.com>
2024-08-21 23:28:43 +0330

    sadsa

--- src/features/core/Header.js
+++ src/features/core/Header.js
@@ -6,7 +6,7 @@ import logo from "../../../assets/images/logo.png"
 
 export function Header(props) {
     let link = props.activeLink;
-   
+
 
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900 mx-2">
@@ -24,12 +24,12 @@ export function Header(props) {
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <Link to="/" className={link == "home" ? 'md:text-blue-700 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500' : ''}>
+                            <Link to="/" className={link == "home" ? 'md:text-blue-700 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}>
                                 Home
                             </Link>
                         </li>
                         <li>
-                            <Link to="/news" className={link == "news" ? 'md:text-blue-700 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500' : ''}>
+                            <Link to="/news" className={link == "news" ? 'md:text-blue-700 block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500' : 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}>
                                 News
                             </Link>
 
